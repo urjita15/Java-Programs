8da760280d1c83bd34e8d1195b6dcdd6191b487c
urjita15 <130883489+urjita15@users.noreply.github.com>
2026-02-07 23:36:53 +0530

    Update MPJ2.java

--- MPJ_2/MPJ2.java
+++ MPJ_2/MPJ2.java
@@ -1,1 +1,16 @@
+public class MPJ2 {
+    public static void main(String[] args) {
+        System.out.println("--- Full Time Employee ---");
+        Employee ft = new FullTimeEmployee(50000);
+        System.out.println("Salary before hike:");
+        ft.DisplaySalary();
+        System.out.println("Salary after 50% hike: " + ft.CalculateSalary());
+
+        System.out.println("\n--- Intern Employee ---");
+        Employee intern = new InternEmployee(20000);
+        System.out.println("Salary before hike:");
+        intern.DisplaySalary();
+        System.out.println("Salary after 25% hike: " + intern.CalculateSalary());
+    }
+}
 
